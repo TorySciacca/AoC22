826b68d7d73b2f9d546d65492b13f99b62da6e39
TorySciacca <torysciacca@gmail.com>
2022-12-23 22:58:39 +1100

    incomplete*

--- day7.py
+++ day7.py
@@ -2,6 +2,22 @@
 
 data = open('day7.txt','r').read().split('\n') #stores each line from the data in a variable 
 
+dirs = []
+path = '/'
+
+for command in data:
+    
+    if command[0] == '$':
+        
+        if command[3:4] == 'cd':
+            pass
+        
+        elif command[3:4] == 'ls':
+            pass
+    
+    else:
+        pass
+     
 #     if l[0:4] == '$ cd':
 #         directory_name = l[4:].strip()
 #         print(directory_name)
