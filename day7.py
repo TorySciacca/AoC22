3c1fc7deb9e0f5642c093a56d923395578dec654
TorySciacca <torysciacca@gmail.com>
2022-12-23 22:53:57 +1100

    day 7 (uncomplete)

---
+++ day7.py
@@ -0,0 +1,21 @@
+"AoC Day 7 - Tory Sciacca"
+
+data = open('day7.txt','r').read().split('\n') #stores each line from the data in a variable 
+
+#     if l[0:4] == '$ cd':
+#         directory_name = l[4:].strip()
+#         print(directory_name)
+#         ls = False
+        
+#     elif l[0:4] == '$ ls':
+#         ls = True
+        
+#     if ls:
+#         a = l.split(' ')
+#         add_to_dir(directory_name,a[1],level)
+
+#     i += 1
+    
+#     if i == 26:
+#         break
+    
