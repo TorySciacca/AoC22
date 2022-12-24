740119a87feec7e7c77e330438047b4878c67fd4
TorySciacca <torysciacca@gmail.com>
2022-12-24 15:46:10 +1100

    Update day7.py

--- day7.py
+++ day7.py
@@ -3,7 +3,7 @@
 data = open('day7.txt','r').read().split('\n') #stores each line from the data in a variable 
 username = 'elf22'
 dirs = []
-path = 'username/'
+path = ''
 
 for command in data: #generate directory 
     
@@ -12,21 +12,24 @@ for command in data: #generate directory
         if command[2:4] == 'cd':
             
             if command[5] == '/': #return to main directory
-                path = 'username/'
-                print(command)
+                path += f'{username}/'
 
             if command[5:7] == '..': #move out a level
                 pass
         
-        elif command[2:4] == 'ls':
+        elif command[2:4] == 'ls': #list command
             pass
     
     else: #directory or file
 
-        if command[:3] == 'dir':
+        if command[:3] == 'dir': #directory found
+            # path += f'{command[3:]}'
             pass
             
-        if command[0].isnumeric():
+        if command[0].isnumeric(): #file found
             pass
         
+#test
+print(path)
+
 #process data
