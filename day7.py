e2765ca5b057d76ea5997dd76b67a797ffefa50a
TorySciacca <torysciacca@gmail.com>
2022-12-26 21:56:21 +1100

    Update day7.py

--- day7.py
+++ day7.py
@@ -6,8 +6,6 @@ username = 'elf22'
 directory = {}
 dir_path = ''
 
-# rfind('/') #returns index of last used of /
-
 for command in data: #generate directory 
     
     if command[0] == '$': #function command
@@ -15,59 +13,31 @@ for command in data: #generate directory
         if command[2:4] == 'cd': #change directory
 
             if command[5:7] == '..': #move out a level
-                current_dir_size = 0
-                
-                for i in directory:
-                    if i == dir_path:
-                        current_dir_size = directory[i]
-                                                
+                current_dir_size = 0                                              
                 prev_dir = dir_path.rfind('/')
                 dir_path = dir_path[:prev_dir]
-                
-                for i in directory:
-                    if i == dir_path:
-                        directory.update({i:directory[i]+current_dir_size})
-            
+                           
             else: 
                 dir_path += str('/' + command[4:].strip())
-                for i in directory:
-                    if i == dir_path:
-                        break
-                directory.update({dir_path:0})
-                # for i in dirs:
-                #     current_dir = command[4:].strip()
-                #     if i == current_dir:
-                #         print(current_dir)
-                        #remove string
-                        #add array ['new string',[]]
-                        # level += 1
+                if dir_path not in directory:
+                    directory.update({dir_path:0})
 
-        
         elif command[2:4] == 'ls': #list command
-            pass
+            continue
     
     else: #directory or file
    
         if command[0].isnumeric(): #file found
+            found = False
             f = command.split(' ') #(f)ile
             for i in directory:
                 if i == f[1]:
-                    break
-            directory.update({i:int(f[0])})
-            # file_ = command.split()
-            # dirs.append(file_[1])
-    
-    # if command == '$ cd cmcrzdt':
-    #     break
+                    found = True
+                    
+            if not found:
+                directory.update({i:int(f[0])})
 
-total_under_10k = 0
+total_under_10k = sum(v for v in directory.values() if v <= 100000)
 
-for i in directory:
-    if directory[i] <= 10000:
-        total_under_10k += directory[i]
-
-#test
 print(directory)
 print(total_under_10k) #solution: 1583951
-
-#process data
